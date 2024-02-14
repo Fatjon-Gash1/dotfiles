a8da920ceba752d91ced33960acc21b6f1a9c00b
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-02-14 14:18:05 +0100

    Create install script

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+ln -s ~/Dotfiles/alacritty/ ~/.config
+ln -s ~/Dotfiles/fish/ ~/.config
+ln -s ~/Dotfiles/hypr/ ~/.config
+ln -s ~/Dotfiles/nvim/ ~/.config
+ln -s ~/Dotfiles/rofi/ ~/.config
+ln -s ~/Dotfiles/scripts/ ~/.config
+ln -s ~/Dotfiles/swaylock/ ~/.config
+ln -s ~/Dotfiles/wal/ ~/.config
+ln -s ~/Dotfiles/waybar/ ~/.config
+
+ln -s ~/Dotfiles/code-flags.conf ~/.config
+ln -s ~/Dotfiles/spotify-flags.conf ~/.config
+ln -s ~/Dotfiles/starship.toml ~/.config
