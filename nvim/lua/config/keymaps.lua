82d4c5794e23e56431063128d4234b99066215c3
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-02-18 14:12:03 +0100

    Edit keymaps.lua

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,7 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- Code tape and move
+-- Code grab and move
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
@@ -10,5 +10,7 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
--- next
-vim.keymap.set("x", "<leader>p", "'_dP")
+vim.api.nvim_set_keymap("n", "<leader>n", ":ASToggle<CR>", {})
+
+-- remove remaping of "ctrl+c" combo with the "esc"
+vim.api.nvim_set_keymap("i", "<C-c>", "<Esc>", { noremap = true, silent = true })
