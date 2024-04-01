637ebbb88a180a892ffd925a29b7cc7e364999f6
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-04-01 16:42:52 +0200

    Change dir path

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dir="$HOME/Images/screenshots/"
+dir="$HOME/Pictures/Screenshots/"
 name="sc_$(date +%d.%m.%Y_%H%M%S).png"
 
 option_1="Area"
@@ -14,21 +14,21 @@ options="$option_1\n$option_2\n$option_3\n$option_4\n$option_5"
 choice=$(echo -e "$options" | rofi -dmenu -i -no-show-icons -l 5 -width 30 -p "Screenshot")
 
 case $choice in
-	$option_1)
-		grim -g "$(slurp)" $dir$name
-		;;
-	$option_2)
-		grim -g "$(slurp)" - | swappy -f - 
-		;;
-	$option_3)
-		sleep 1
-		grim $dir$name
-		;;
-	$option_4)
-		sleep 1
-		grim - | swappy -f - 
-		;;
-	$option_5)
-		wl-color-picker clipboard
-		;;
+$option_1)
+	grim -g "$(slurp)" $dir$name
+	;;
+$option_2)
+	grim -g "$(slurp)" - | swappy -f -
+	;;
+$option_3)
+	sleep 1
+	grim $dir$name
+	;;
+$option_4)
+	sleep 1
+	grim - | swappy -f -
+	;;
+$option_5)
+	wl-color-picker clipboard
+	;;
 esac
