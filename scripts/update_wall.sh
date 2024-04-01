6e49f5699d513f3eedd93d36d6be4a61874b8df2
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-04-01 16:42:03 +0200

    Set animation to none & change dir path

--- scripts/update_wall.sh
+++ scripts/update_wall.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 # Select random walpaper
-wal -q -i ~/Images/wallpapers/
+wal -q -i ~/Pictures/Wallpapers/
 
 # Load current pywal color scheme
 source "$HOME/.cache/wal/colors.sh"
 
 # Coply color file to waybar folder
-cp ~/.cache/wal/colors-waybar.css ~/.config/waybar/
+# cp ~/.cache/wal/colors-waybar.css ~/.config/waybar/
 
 # Set the new wallpaper
-swww img $wallpaper --transition-type wipe --transition-duration 1 --transition-fps 30 --transition-bezier .42,0,.58,1
-~/.config/waybar/launch_wb.sh
+swww img $wallpaper --transition-type none
