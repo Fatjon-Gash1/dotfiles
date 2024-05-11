d3a936f2e8b52857fce3099e075656974edc4e6e
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-05-11 11:07:41 +0200

    Add sink occurrences

--- scripts/changeAudio.sh
+++ scripts/changeAudio.sh
@@ -2,10 +2,10 @@
 
 currentSink=$(pamixer --get-default-sink)
 
-if [[ $currentSink == *"52"* ]]; then
+if [[ $currentSink == *"52"* || $currentSink == *"55"* ]]; then
 	pactl set-default-sink 66
 elif [[ $currentSink == *"66"* ]]; then
-	pactl set-default-sink 52
+	pactl set-default-sink 52 || pactl set-default-sink 55
 else
 	echo "current sink is: ${currentSink}"
 fi
