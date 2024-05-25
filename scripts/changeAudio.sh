a500a0bcb2a2ca1a77821b752349d778f65fe2a9
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-05-25 12:21:49 +0200

    Add new port

--- scripts/changeAudio.sh
+++ scripts/changeAudio.sh
@@ -3,8 +3,8 @@
 currentSink=$(pamixer --get-default-sink)
 
 if [[ $currentSink == *"52"* || $currentSink == *"55"* ]]; then
-	pactl set-default-sink 66
-elif [[ $currentSink == *"66"* ]]; then
+	pactl set-default-sink 66 || pactl set-default-sink 71
+elif [[ $currentSink == *"66"* || $currentSink == *"71"* ]]; then
 	pactl set-default-sink 52 || pactl set-default-sink 55
 else
 	echo "current sink is: ${currentSink}"
