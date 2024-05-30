2ee9ed99bf97c781baab7c04f8d79bd0402a4dd0
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-05-30 23:15:54 +0200

    Reorder aliases

--- fish/config.fish
+++ fish/config.fish
@@ -26,13 +26,13 @@ if status is-interactive
     alias ll 'exa --header --long --extended --group-directories-first --no-user --no-permissions --icons'
     alias v nvim
     alias rbackup 'sudo rsync -avh --progress --exclude={"/data/*","/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found"} --delete / /data/backup/'
-    alias ga 'git add'
     alias gs 'git status'
-    alias gp 'git pull'
-    alias gps 'git push'
+    alias ga 'git add'
     alias gc 'git commit -m'
-    alias gr 'git rm'
+    alias gps 'git push'
+    alias gp 'git pull'
     alias gm 'git mv'
+    alias gr 'git rm'
 
     # Themes
     #set -g theme_display_user yes
