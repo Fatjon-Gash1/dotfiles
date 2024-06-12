63252b43ebf9c2cd4b87f8451dd92a72e9e5553c
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-06-12 19:40:33 +0200

    Add linters & formatters

---
+++ nvim/lua/pluginsConf/formatter.lua
@@ -0,0 +1,21 @@
+require("conform").setup({
+	formatters_by_ft = {
+		javascript = { "prettier" },
+		typescript = { "prettier" },
+		javascriptreact = { "prettier" },
+		typescriptreact = { "prettier" },
+		svelte = { "prettier" },
+		css = { "prettier" },
+		html = { "prettier" },
+		json = { "prettier" },
+		yaml = { "prettier" },
+		markdown = { "prettier" },
+		graphql = { "prettier" },
+		lua = { "stylua" },
+	},
+	format_on_save = {
+		async = false,
+		timeout_ms = 500,
+		lsp_fallback = true,
+	},
+})
