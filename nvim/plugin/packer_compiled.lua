614676905ab7aa63cf14fc7aff627ca175ed055b
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-06-12 21:12:12 +0200

    Add coloriser plugin

--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -154,6 +154,11 @@ _G.packer_plugins = {
     path = "/home/fatjon/.local/share/nvim/site/pack/packer/start/nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
   },
+  ["nvim-colorizer.lua"] = {
+    loaded = true,
+    path = "/home/fatjon/.local/share/nvim/site/pack/packer/start/nvim-colorizer.lua",
+    url = "https://github.com/norcalli/nvim-colorizer.lua"
+  },
   ["nvim-lint"] = {
     loaded = true,
     path = "/home/fatjon/.local/share/nvim/site/pack/packer/start/nvim-lint",
