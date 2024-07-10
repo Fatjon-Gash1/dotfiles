df6c143b20b2e66f21cb75794fe5b4a75cbf3076
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-07-10 20:48:33 +0200

    Create sym link for ranger

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 ln -s ~/dotfiles/i3/ ~/.config
 ln -s ~/dotfiles/polybar/ ~/.config
 ln -s ~/dotfiles/picom/ ~/.config
+ln -s ~/dotfiles/ranger/ ~/.config
 ln -s ~/dotfiles/alacritty/ ~/.config
 ln -s ~/dotfiles/fish/ ~/.config
 ln -s ~/dotfiles/nvim/ ~/.config
