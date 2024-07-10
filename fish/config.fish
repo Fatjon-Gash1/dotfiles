4a75160a055f091996433c96f3a9f8963047254d
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-07-10 19:29:25 +0200

    Change wm to i3

--- fish/config.fish
+++ fish/config.fish
@@ -1,31 +1,17 @@
 set fish_greeting
 
-# Set GNOME schema
-set gnome_schema org.gnome.desktop.interface
-
-# Set GTK theme, icon theme, cursor theme, and font name
-gsettings set $gnome_schema gtk-theme Arc-Dark
-gsettings set $gnome_schema icon-theme Papirus-Dark
-gsettings set $gnome_schema cursor-theme Adwaita
-gsettings set $gnome_schema font-name 'Monospace 11'
-
-# Set pywal colors for the terminal
-cat ~/.cache/wal/sequences
-
-
 if status is-interactive
 
     # Aliases
     alias cl clear
     alias op open
-    alias cd.. 'cd ..'
     alias md mkdir
     alias rd rmdir
     alias t touch
     alias fv 'find . -type f | peco | xargs nvim'
     alias ll 'exa --header --long --extended --group-directories-first --no-user --no-permissions --icons'
     alias v nvim
-    alias rbackup 'sudo rsync -avh --progress --exclude={"/data/*","/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found"} --delete / /data/backup/'
+    alias s startx
     alias gs 'git status'
     alias ga 'git add'
     alias gc 'git commit -m'
