b682daffadd018578b98e2da21dcf2b67e0c0d69
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-07-10 19:43:15 +0200

    Add sym links

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+ln -s ~/dotfiles/i3/ ~/.config
+ln -s ~/dotfiles/polybar/ ~/.config
+ln -s ~/dotfiles/picom/ ~/.config
 ln -s ~/dotfiles/alacritty/ ~/.config
 ln -s ~/dotfiles/fish/ ~/.config
 ln -s ~/dotfiles/nvim/ ~/.config
