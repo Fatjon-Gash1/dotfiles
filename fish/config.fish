e8395acff2d02fafbd564509e39491a2ab97fbc6
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-07-12 19:19:28 +0200

    Re-add rbackup alias

--- fish/config.fish
+++ fish/config.fish
@@ -10,6 +10,7 @@ if status is-interactive
     alias t touch
     alias fv 'find . -type f | peco | xargs nvim'
     alias ll 'exa --header --long --extended --group-directories-first --no-user --no-permissions --icons'
+    alias rbackup 'sudo rsync -avh --progress --exclude={"/data/*","/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found"} --delete / /data/backup/'
     alias v nvim
     alias s startx
     alias gs 'git status'
