cedcbab701ecac9adfe05554fd091fe33218c90d
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-10-18 17:30:47 +0200

    Add null_ls & cspell

--- nvim/lua/pluginsConf/lsp.lua
+++ nvim/lua/pluginsConf/lsp.lua
@@ -25,22 +25,36 @@ require("mason-tool-installer").setup({
 	},
 })
 
+local null_ls = require("null-ls")
+
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
 local on_attach = function(_, _)
 	vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
 	vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
 
-	vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
 	vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, {})
 	vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 end
 
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
 -- Only for lsps that don't detect root
 local dirFind = function()
 	return vim.loop.cwd()
 end
+
+null_ls.setup({
+	on_attach = on_attach,
+	sources = {
+		null_ls.builtins.diagnostics.cspell.with({
+			diagnostics_postprocess = function(diagnostic)
+				diagnostic.severity = vim.diagnostic.severity.WARN
+			end,
+			filetypes = { "markdown", "text", "javascript", "typescript", "html", "css" },
+		}),
+		null_ls.builtins.code_actions.cspell,
+	},
+})
 
 require("lspconfig").lua_ls.setup({
 	on_attach = on_attach,
