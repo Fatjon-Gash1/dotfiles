772c4ade3bd386f6c3035e30a1f053e131891e7b
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2025-03-30 21:49:48 +0200

    Remove old files

--- install.sh
+++ install.sh
@@ -7,11 +7,9 @@ ln -s ~/dotfiles/alacritty/ ~/.config
 ln -s ~/dotfiles/fish/ ~/.config
 ln -s ~/dotfiles/nvim/ ~/.config
 ln -s ~/dotfiles/rofi/ ~/.config
-ln -s ~/dotfiles/scripts/ ~/.config
 ln -s ~/dotfiles/feh/ ~/.config
 
 ln -s ~/dotfiles/code-flags.conf ~/.config
-ln -s ~/dotfiles/spotify-flags.conf ~/.config
 ln -s ~/dotfiles/starship.toml ~/.config
 ln -s ~/dotfiles/.tmux.conf ~/
 ln -s ~/dotfiles/.xinitrc ~/
