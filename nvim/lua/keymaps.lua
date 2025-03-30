5775de8d83188a1ad7f080c2bcfcaa510b81bc76
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2025-03-30 22:06:42 +0200

    Remove formatting on save, add manual formatting keymap

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,3 +1,15 @@
+local format_function = function(args)
+	local range = nil
+	if args.count ~= -1 then
+		local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+		range = {
+			start = { args.line1, 0 },
+			["end"] = { args.line2, end_line:len() },
+		}
+	end
+	require("conform").format({ async = true, lsp_format = "fallback", range = range })
+end
+
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "n", "nzzzv")
@@ -8,4 +20,12 @@ vim.keymap.set("n", "<c-n>", ":NvimTreeFindFileToggle<CR>")
 vim.keymap.set("n", "<a-f>", ":NvimTreeFocus<CR>")
 vim.keymap.set("n", "<leader>e", function()
 	vim.diagnostic.open_float(nil, { focus = false })
+end, { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>f", function()
+	local args = {
+		count = vim.v.count,
+		line1 = vim.fn.line("w0"),
+		line2 = vim.fn.line("w$"),
+	}
+	format_function(args)
 end, { noremap = true, silent = true })
