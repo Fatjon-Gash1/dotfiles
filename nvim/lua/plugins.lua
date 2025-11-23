d9e9e3e952bf9668f7ddfb12554d71f9391cb9e2
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2025-11-23 13:20:30 +0100

    Add dap, dap ui, and dap virtual text

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -56,6 +56,12 @@ return require("packer").startup(function(use)
 		"nvimtools/none-ls-extras.nvim",
 	})
 
+    -- dap
+    use("mfussenegger/nvim-dap")
+    use("rcarriga/nvim-dap-ui")
+    use("theHamsta/nvim-dap-virtual-text")
+    use("nvim-neotest/nvim-nio")
+
 	if packer_bootstrap then
 		require("packer").sync()
 	end
