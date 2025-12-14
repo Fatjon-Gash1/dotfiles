19e9f39e8acf4739fc9ef18a126babbbd95ac921
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2025-12-14 20:34:56 +0100

    chore: remove unnecessary macos packages from installation

--- install.sh
+++ install.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
-ln -s ~/dotfiles/i3/ ~/.config
-ln -s ~/dotfiles/polybar/ ~/.config
-ln -s ~/dotfiles/picom/ ~/.config
-ln -s ~/dotfiles/ranger/ ~/.config
 ln -s ~/dotfiles/alacritty/ ~/.config
 ln -s ~/dotfiles/fish/ ~/.config
 ln -s ~/dotfiles/nvim/ ~/.config
-ln -s ~/dotfiles/rofi/ ~/.config
-ln -s ~/dotfiles/feh/ ~/.config
 
-ln -s ~/dotfiles/code-flags.conf ~/.config
 ln -s ~/dotfiles/starship.toml ~/.config
 ln -s ~/dotfiles/.tmux.conf ~/
-ln -s ~/dotfiles/.xinitrc ~/
-sudo ln -s ~/dotfiles/reflector.conf /etc/xdg/reflector
