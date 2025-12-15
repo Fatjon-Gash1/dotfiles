c4b2d3f3d82a39ea2decf506df2c078d49a15e61
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2025-12-15 10:34:09 +0100

    chore: Add auto-indent treesitter as handller, configure php indent level

--- nvim/lua/pluginsConf/treesitter.lua
+++ nvim/lua/pluginsConf/treesitter.lua
@@ -2,5 +2,18 @@ require'nvim-treesitter'.install { 'lua', 'typescript', 'javascript', 'php', 'tw
 
 vim.api.nvim_create_autocmd('FileType', {
   pattern = { 'lua', 'typescript', 'javascript', 'php', 'twig', 'scss' },
-  callback = function() vim.treesitter.start() end,
+  callback = function()
+      vim.treesitter.start()
+      vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+  end,
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'php',
+  callback = function()
+    vim.bo.shiftwidth = 2
+    vim.bo.tabstop = 2
+    vim.bo.softtabstop = 2
+    vim.bo.expandtab = true
+  end,
 })
