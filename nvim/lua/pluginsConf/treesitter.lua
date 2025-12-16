f4d28506d959a8c1a9291a4979b472a7fba7e8a8
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2025-12-16 12:41:52 +0100

    chore: Remove 2 line auto-indentation for php

--- nvim/lua/pluginsConf/treesitter.lua
+++ nvim/lua/pluginsConf/treesitter.lua
@@ -7,13 +7,3 @@ vim.api.nvim_create_autocmd('FileType', {
       vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
   end,
 })
-
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = 'php',
-  callback = function()
-    vim.bo.shiftwidth = 2
-    vim.bo.tabstop = 2
-    vim.bo.softtabstop = 2
-    vim.bo.expandtab = true
-  end,
-})
