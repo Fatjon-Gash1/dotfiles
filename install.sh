8d48063288d21d985e7ee1e833eb85a65fce9d9e
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2026-04-14 22:27:31 +0200

    feat: add prettier config

--- install.sh
+++ install.sh
@@ -13,4 +13,5 @@ ln -s ~/dotfiles/code-flags.conf ~/.config
 ln -s ~/dotfiles/starship.toml ~/.config
 ln -s ~/dotfiles/.tmux.conf ~/
 ln -s ~/dotfiles/.xinitrc ~/
+ln -s ~/dotfiles/.prettierrc ~/
 sudo ln -s ~/dotfiles/reflector.conf /etc/xdg/reflector
