6ad58f3ad4ae61a677e34445a520f1cb897b7e7b
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2026-04-14 22:31:28 +0200

    chore: add python parser

--- nvim/lua/pluginsConf/treesitter.lua
+++ nvim/lua/pluginsConf/treesitter.lua
@@ -1,7 +1,7 @@
-require'nvim-treesitter'.install { 'lua', 'typescript', 'javascript', 'php', 'twig', 'scss' }
+require'nvim-treesitter'.install { 'lua', 'typescript', 'javascript', 'php', 'twig', 'scss', 'python' }
 
 vim.api.nvim_create_autocmd('FileType', {
-  pattern = { 'lua', 'typescript', 'javascript', 'php', 'twig', 'scss' },
+  pattern = { 'lua', 'typescript', 'javascript', 'php', 'twig', 'scss', 'python' },
   callback = function()
       vim.treesitter.start()
       vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
