184110d2cc7bb6a26f6e6df477b06ef75a7a164a
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2026-05-25 18:02:53 +0200

    chore(nvim): add "markdown" and "markdown_inline" parsers

--- nvim/lua/pluginsConf/treesitter.lua
+++ nvim/lua/pluginsConf/treesitter.lua
@@ -1,9 +1,19 @@
-require'nvim-treesitter'.install { 'lua', 'typescript', 'javascript', 'php', 'twig', 'scss', 'python' }
+require("nvim-treesitter").install({
+	"lua",
+	"typescript",
+	"javascript",
+	"php",
+	"twig",
+	"scss",
+	"python",
+	"markdown",
+	"markdown_inline",
+})
 
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = { 'lua', 'typescript', 'javascript', 'php', 'twig', 'scss', 'python' },
-  callback = function()
-      vim.treesitter.start()
-      vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-  end,
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "lua", "typescript", "javascript", "php", "twig", "scss", "python", "markdown", "markdown_inline" },
+	callback = function()
+		vim.treesitter.start()
+		vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+	end,
 })
