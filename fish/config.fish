b3891ac8be841c8a6983f33106ac8eb9f88517c5
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2026-07-15 23:06:00 +0200

    chore(fish): update rsync alias to optimal system restore backup and new machine migrations

--- fish/config.fish
+++ fish/config.fish
@@ -8,7 +8,9 @@ alias rd rmdir
 alias t touch
 alias fv 'find . -type f | peco | xargs nvim'
 alias ll 'exa --header --long --extended --group-directories-first --no-user --no-permissions --icons'
-alias rbackup 'sudo rsync -avh --progress --exclude={"/data/*","/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found"} --delete / /data/backup/'
+alias rbackup 'sudo rsync -aAXHS --numeric-ids --delete --delete-excluded --info=progress2 \
+  --exclude={"/data/***","/dev/***","/proc/***","/sys/***","/tmp/***","/run/***","/mnt/***","/media/***","/lost+found","/swapfile","/var/tmp/***","/var/cache/pacman/pkg/***","/var/lib/systemd/coredump/***","/home/*/.cache/***"} \
+  / /data/backup/'
 alias v nvim
 alias s startx
 alias gs 'git status'
